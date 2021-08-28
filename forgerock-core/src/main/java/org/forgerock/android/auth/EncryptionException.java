33b4252c3189bbe7b5613458e335e87e900e53c5
Andy Witrisna <witrisna@gmail.com>
2021-08-28 00:09:30 -0700

    SDKS-1141 Remove unnecessary constructor for EncryptionException

--- forgerock-core/src/main/java/org/forgerock/android/auth/EncryptionException.java
+++ forgerock-core/src/main/java/org/forgerock/android/auth/EncryptionException.java
@@ -27,7 +27,4 @@ public class EncryptionException extends RuntimeException {
         super(cause);
     }
 
-    public EncryptionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
