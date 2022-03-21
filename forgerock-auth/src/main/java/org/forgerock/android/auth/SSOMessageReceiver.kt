b88216bb53976acbde0e6de34f9d440ab14c902f
jey <jey.periyasamy@forgerock.com>
2022-03-21 14:02:01 -0500

    Enabling kotlin in the auth library

---
+++ forgerock-auth/src/main/java/org/forgerock/android/auth/SSOMessageReceiver.kt
@@ -0,0 +1,11 @@
+package org.forgerock.android.auth
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SSOMessageReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
