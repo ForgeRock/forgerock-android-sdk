79e8e568f0c0695a5454e6ad70df74113a1a4ed8
andy witrisna <witrisna@gmail.com>
2023-01-23 09:01:38 -0800

    SDKS-2293 Developer interface to unbind device

---
+++ forgerock-auth/src/main/java/org/forgerock/android/auth/FRUserKeys.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 ForgeRock. All rights reserved.
+ *
+ *  This software may be modified and distributed under the terms
+ *  of the MIT license. See the LICENSE file for details.
+ */
+
+package org.forgerock.android.auth
+
+import android.content.Context
+import org.forgerock.android.auth.callback.DeviceBindingCallback
+import org.forgerock.android.auth.devicebind.UserDeviceKeyService
+import org.forgerock.android.auth.devicebind.UserKey
+import org.forgerock.android.auth.devicebind.UserKeyService
+
+/**
+ * Manage [UserKey] that created by the SDK
+ * The [UserKey] are created with [DeviceBindingCallback]
+ */
+class FRUserKeys(private val context: Context,
+                 private val userKeyService: UserKeyService = UserDeviceKeyService(context)) {
+
+    /**
+     * Load all the [UserKey] which created with [DeviceBindingCallback]
+     * @return All the [UserKey]
+     */
+    fun loadAll(): List<UserKey> {
+        return userKeyService.userKeys
+    }
+
+    /**
+     * Delete the [UserKey]
+     * @param userKey The [UserKey] to be deleted
+     */
+    fun delete(userKey: UserKey) {
+        userKeyService.delete(userKey)
+    }
+
+}
