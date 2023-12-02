d952eb46c857068ead77899ce6f9e91148b7b2a9
Stoyan Petrov <stoyan@petrov.ca>
2023-12-01 17:26:59 -0800

    Added e2e tests for the Android App Integrity feature.

--- forgerock-auth-ui/src/main/java/org/forgerock/android/auth/ui/CallbackFragmentFactory.java
+++ forgerock-auth-ui/src/main/java/org/forgerock/android/auth/ui/CallbackFragmentFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 - 2022 ForgeRock. All rights reserved.
+ * Copyright (c) 2019 - 2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -58,6 +58,7 @@ public class CallbackFragmentFactory {
         register(SuspendedTextOutputCallback.class, SuspendedTextOutputCallbackFragment.class);
         register(ReCaptchaCallback.class, ReCaptchaCallbackFragment.class);
         register(ConsentMappingCallback.class, ConsentMappingCallbackFragment.class);
+        register(AppIntegrityCallback.class, AppIntegrityCallbackFragment.class);
         register(DeviceProfileCallback.class, DeviceProfileCallbackFragment.class);
         register(DeviceBindingCallback.class, DeviceBindingCallbackFragment.class);
         register(DeviceSigningVerifierCallback.class, DeviceSigningVerifierCallbackFragment.class);
