8d16c67c28c0d91b39fd7732bb208837f0533ef0
Stoyan Petrov <stoyan@petrov.ca>
2024-03-26 13:50:56 -0700

    Fixed certificate pinning unit tests

--- forgerock-auth/src/test/java/org/forgerock/android/auth/BrowserLoginTest.kt
+++ forgerock-auth/src/test/java/org/forgerock/android/auth/BrowserLoginTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 - 2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2020 - 2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -175,7 +175,7 @@ class BrowserLoginTest : BaseTest() {
     }
 
     //It is running with JVM, no browser is expected
-    @Test(expected = ActivityNotFoundException::class)
+    @Test
     @Throws(
         InterruptedException::class,
         ExecutionException::class,
